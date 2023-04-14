472201bbcff19a4b2ca2334a644b200a647e5eaa
ONE Webdesign <info@onewebdesign.nl>
2023-04-14 23:42:47 +0200

    UPDATE: ignore build errors

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,7 @@
 /** @type {import("next").NextConfig} */
 const config = {
   reactStrictMode: false,
+  ignoreBuildErrors: true,
 
   /**
    * If you have the "experimental: { appDir: true }" setting enabled, then you
