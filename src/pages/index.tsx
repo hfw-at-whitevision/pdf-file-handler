7e5f3f9e365d15c38197c075b9a6ee1130363bf7
Ho-Feng <Ho-Feng.Wong@whitevision.nl>
2023-04-20 12:27:25 +0200

    ADD: multi PDFs support

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -28,9 +28,7 @@ function downloadURI(uri: string, name: string) {
 
 const Home: NextPage = () => {
   const [originalPdf, setOriginalPdf] = useState(null);
-  const [pdf, setPdf]: any = useState(null);
-  const [position, setPosition] = useState(null);
-  const [textInputVisible, setTextInputVisible] = useState(false);
+  const [pdf, setPdfs]: [Array<string>, any] = useState(null);
   const [pageNum, setPageNum] = useState(0);
   const [totalPages, setTotalPages] = useState(0);
   const [pageDetails, setPageDetails] = useState(null);
@@ -42,54 +40,53 @@ const Home: NextPage = () => {
 
   useEffect(() => {
     setCurrentRotation(0);
-  }, [pageNum])
+  }, [pageNum]);
 
   const handleReset = async (e) => {
     e.preventDefault();
-    //                setTextInputVisible(false);
-    setPdf(originalPdf);
+    setPdfs([originalPdf]);
     //                      setTotalPages(0);
     setPageNum(0);
     setCurrentRotation(0);
     //                  setPageDetails(null);
-  }
+  };
 
   const handleRotatePage = async (inputDegrees: number = 90) => {
     setIsRotating(true);
     const pdfDoc = await PDFDocument.load(pdf, {
-      ignoreEncryption: true,
+      ignoreEncryption: true
     });
     const pages = pdfDoc.getPages();
     const currentPage: any = pages[pageNum];
-    if (currentPage) currentPage.setRotation(degrees(currentRotation + inputDegrees))
+    if (currentPage) currentPage.setRotation(degrees(currentRotation + inputDegrees));
     setCurrentRotation(currentRotation + inputDegrees);
     const pdfBytes = await pdfDoc.save();
     const blob = new Blob([new Uint8Array(pdfBytes)]);
     const URL = await blobToURL(blob);
-    setPdf(URL);
+    setPdfs(URL);
     setIsRotating(false);
-  }
+  };
 
   const handleRemovePage = async (inputPageNum: number = pageNum) => {
     setIsDeleting(true);
     const pdfDoc = await PDFDocument.load(pdf, {
-      ignoreEncryption: true,
+      ignoreEncryption: true
     });
     pdfDoc.removePage(inputPageNum);
-    let newPageNum = inputPageNum === totalPages - 1 ? inputPageNum - 1 : inputPageNum
-    newPageNum = newPageNum < 0 ? 0 : newPageNum
-    setPageNum(newPageNum)
+    let newPageNum = inputPageNum === totalPages - 1 ? inputPageNum - 1 : inputPageNum;
+    newPageNum = newPageNum < 0 ? 0 : newPageNum;
+    setPageNum(newPageNum);
     setCurrentRotation(0);
     const pdfBytes = await pdfDoc.save();
     const blob = new Blob([new Uint8Array(pdfBytes)]);
     const URL = await blobToURL(blob);
-    setPdf(URL);
+    setPdfs(URL);
     setIsDeleting(false);
-  }
+  };
 
   const handleMovePage = async (newIndex: number = pageNum) => {
     const pdfDoc = await PDFDocument.load(pdf, {
-      ignoreEncryption: true,
+      ignoreEncryption: true
     });
     const [currentPage]: any = await pdfDoc.copyPages(pdfDoc, [pageNum]);
     pdfDoc.insertPage(newIndex, currentPage);
@@ -98,20 +95,20 @@ const Home: NextPage = () => {
     const pdfBytes = await pdfDoc.save();
     const blob = new Blob([new Uint8Array(pdfBytes)]);
     const URL = await blobToURL(blob);
-    setPdf(URL);
+    setPdfs(URL);
     setPageNum(pageNum + 1);
-  }
+  };
 
-  let thumbnails: any[] = []
+  let thumbnails: any[] = [];
   for (let i = 0; i < totalPages; i++) {
     thumbnails = [...thumbnails,
-    <Page
-      className={`rounded-md overflow-hidden max-h-[200px] cursor-pointer mr-4 ${i === pageNum ? 'border-4 border-amber-300' : ''}`}
-      pageNumber={i + 1}
-      width={180}
-      onClick={() => setPageNum(i)}
-    />
-    ]
+      <Page
+        className={`rounded-md overflow-hidden max-h-[200px] cursor-pointer mr-4 ${i === pageNum ? "border-4 border-amber-300" : ""}`}
+        pageNumber={i + 1}
+        width={180}
+        onClick={() => setPageNum(i)}
+      />
+    ];
   }
 
   return (
@@ -127,12 +124,12 @@ const Home: NextPage = () => {
 
         <div className={
           `flex gap-12 px-4 py-16
-          ${pdf ? 'flex-row' : 'flex-col items-center justify-center'}`
+          ${pdf ? "flex-row" : "flex-col items-center justify-center"}`
         }>
 
           <header className={
             `flex flex-col
-            ${!pdf ? 'items-center' : 'max-w-sm'}
+            ${!pdf ? "items-center" : "max-w-sm"}
             `
           }>
             <img src="./whitevision.png" width={150} className="flex justify-center gap-2 text-lg mb-4" />
@@ -142,16 +139,19 @@ const Home: NextPage = () => {
             <h5 className="text-white font-extrabold uppercase ml-1 text-sm mt-2">product owned by Jasper B.</h5>
             <div className={
               `grid gap-4 mt-6
-              ${!pdf ? 'grid-cols-1' : 'grid-cols-1'}
+              ${!pdf ? "grid-cols-1" : "grid-cols-1"}
               `
             }>
               <Drop
                 onLoaded={async (files: any) => {
-                  const URL = await blobToURL(files[0]);
-                  setPdf(URL);
-                  setOriginalPdf(URL);
+                  const newPdf = await blobToURL(files[0]);
+                  setPdfs((oldPdfs) => {
+                    const result = oldPdfs ? oldPdfs.concat(newPdf) : [newPdf]
+                    return result;
+                  });
+                  setOriginalPdf(newPdf);
                 }}
-                className={pdf ? 'opacity-50' : '!p-16'}
+                className={pdf ? "opacity-50" : "!p-16"}
               />
               {pdf
                 ? <>
@@ -164,7 +164,7 @@ const Home: NextPage = () => {
                   <ButtonXl
                     title="Download"
                     description="Creëer en download het PDF bestand."
-                    className={pdf ? '' : 'opacity-40 pointer-events-none'}
+                    className={pdf ? "" : "opacity-40 pointer-events-none"}
                     onClick={() => {
                       downloadURI(pdf, "pdffilehandler.pdf");
                     }}
@@ -175,16 +175,12 @@ const Home: NextPage = () => {
           </header>
 
           {pdf ? (
-            <div>
-              <nav className={`${isLoading ? 'disabled' : ''} flex gap-1 mb-2`}>
+            <div style={{ color: "white" }}>
+              <nav className={`${isLoading ? "disabled" : ""} flex gap-1 mb-2`}>
                 <BigButton
                   title={<><BsArrowDown /> Pagina omlaag</>}
                   onClick={() => handleMovePage(pageNum + 2)}
                   disabled={isDeleting}
-                />
-                <BigButton
-                  title={<><BsPlusLg /> Tekst toevoegen</>}
-                  onClick={() => setTextInputVisible(true)}
                 />
                 <BigButton
                   title={<><GrRotateRight /> Roteer 90°</>}
@@ -200,81 +196,50 @@ const Home: NextPage = () => {
 
               <main
                 ref={documentRef}
-                className={`${(window.innerWidth < 1200) ? `w-[${window.innerWidth * 0.75}px]` : 'w-[1000px] flex flex-row gap-2'}`}
+                className={`${(window.innerWidth < 1200) ? `w-[${window.innerWidth * 0.75}px]` : "w-[1000px] flex flex-col gap-2"}`}
               >
-                {textInputVisible ? (
-                  <DraggableText
-                    onCancel={() => setTextInputVisible(false)}
-                    onEnd={setPosition}
-                    onSet={async (text: any) => {
-                      const { originalHeight, originalWidth }: any = pageDetails;
-                      const scale = 1; // originalWidth / documentRef?.current?.clientWidth
 
-                      const y =
-                        documentRef?.current?.clientHeight -
-                        (position?.y +
-                          (12 * scale) -
-                          position?.offsetY -
-                          documentRef?.current?.offsetTop);
-                      const x =
-                        position?.x -
-                        166 -
-                        position?.offsetX -
-                        documentRef?.current?.offsetLeft;
+                {
+                  (pdf?.length) &&
+                  pdf?.map((pdfDoc, i) => <>
 
-                      // new XY in relation to actual document size
-                      const newY =
-                        (y * originalHeight) / documentRef?.current?.clientHeight;
-                      const newX =
-                        (x * originalWidth) / documentRef?.current?.clientWidth;
+                    <Document
+                      key={`pdfDoc-${i}`}
+                      file={pdfDoc}
+                      loading={<Loading />}
+                      onLoadSuccess={(data) => {
+                        setTotalPages(data.numPages);
+                      }}
+                      className="w-[800px]"
+                    >
+                      <Page
+                        className="rounded-lg overflow-hidden"
+                        pageNumber={pageNum + 1}
+                        width={(window.innerWidth < 1200) ? window.innerWidth * 0.75 : 800}
+                        onLoadSuccess={(data) => {
+                          setPageDetails(data);
+                        }}
+                      />
 
-                      const pdfDoc = await PDFDocument.load(pdf, {
-                        ignoreEncryption: true,
-                      });
+                      thumbnails = [...thumbnails,
+                      <Page
+                        className={`rounded-md overflow-hidden max-h-[200px] cursor-pointer mr-4 ${i === pageNum ? "border-4 border-amber-300" : ""}`}
+                        pageNumber={i + 1}
+                        width={180}
+                        onClick={() => setPageNum(i)}
+                      />
+                      ];
 
-                      const pages = pdfDoc.getPages();
-                      const firstPage = pages[pageNum];
+                      <PagingControl
+                        pageNum={pageNum}
+                        setPageNum={setPageNum}
+                        totalPages={totalPages}
+                      />
+                    </Document>
 
-                      firstPage?.drawText(text, {
-                        x: newX,
-                        y: newY,
-                        size: 20 * scale,
-                      });
-
-                      const pdfBytes = await pdfDoc.save();
-                      const blob = new Blob([new Uint8Array(pdfBytes)]);
-
-                      const URL = await blobToURL(blob);
-                      setPdf(URL);
-                      setPosition(null);
-                      setTextInputVisible(false);
-                    }}
-                  />
-                ) : null}
-                <Document
-                  file={pdf}
-                  loading={<Loading />}
-                  onLoadSuccess={(data) => {
-                    setTotalPages(data.numPages);
-                  }}
-                  className="w-[800px]"
-                >
-                  <Page
-                    className="rounded-lg overflow-hidden"
-                    pageNumber={pageNum + 1}
-                    width={(window.innerWidth < 1200) ? window.innerWidth * 0.75 : 800}
-                    onLoadSuccess={(data) => {
-                      setPageDetails(data);
-                    }}
-                  />
-
-                  <PagingControl
-                    pageNum={pageNum}
-                    setPageNum={setPageNum}
-                    totalPages={totalPages}
-                  />
-                </Document>
-
+                  </>)
+                }
+                {/*
                 <section className={`w-[200px] overflow-y-auto overflow-x-hidden h-[600px] rounded-md relative`}>
                   <Document
                     file={pdf}
@@ -285,12 +250,14 @@ const Home: NextPage = () => {
                     </div>
                   </Document>
                 </section>
+                */}
               </main>
             </div>
           ) : null}
         </div>
 
-        <div className="fixed inset-0 bg-gradient-to-b from-transparent via-transparent to-black/50 z-10 pointer-events-none" />
+        <div
+          className="fixed inset-0 bg-gradient-to-b from-transparent via-transparent to-black/50 z-10 pointer-events-none" />
       </div>
     </>
   );
